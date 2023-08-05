e7bb194b5e67646eb26d34f958f72d77c9f997f5
Aaron Randall <aaronasherrandall@gmail.com>
2023-08-05 17:58:42 +0900

    updated

--- app/models/doc.rb
+++ app/models/doc.rb
@@ -1,3 +1,7 @@
 class Doc < ApplicationRecord
   belongs_to :user
+
+  has_many :doc_chats
+  validates :file_name, :contents, :file_type, presence: true
+  validates :character_count, length: { maximum: 4000 }
 end
