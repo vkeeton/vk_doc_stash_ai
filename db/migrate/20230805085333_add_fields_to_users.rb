b5406f374693aba517212bcb3fa62b4358051b3c
Aaron Randall <aaronasherrandall@gmail.com>
2023-08-05 17:54:02 +0900

    updated models

--- db/migrate/20230805085333_add_fields_to_users.rb
+++ db/migrate/20230805085333_add_fields_to_users.rb
@@ -2,7 +2,7 @@ class AddFieldsToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
-    add_column :users, :usr_name, :string
+    add_column :users, :user_name, :string
     add_column :users, :job_title, :string
     add_column :users, :avatar, :string
   end
