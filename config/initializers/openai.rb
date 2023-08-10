b5e1dc6f0815a6014ff61510b71300a8020a19a1
Aaron Randall <aaronasherrandall@gmail.com>
2023-08-10 20:43:42 +0900

    fixed issue with open ai service

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,4 +1,4 @@
 OpenAI.configure do |config|
-  config.access_token = ENV.fetch("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")
+  config.access_token = ENV.fetch("OPENAI_ACCESS_TOKEN")
   config.request_timeout = 240 # Optional
 end
