3e080edc09b368d0901a4741d58513a09661e46a
Aaron Randall <aaronasherrandall@gmail.com>
2023-08-22 22:08:25 +0900

    edited message controller to get multiple docs working

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,8 @@
 class MessagesController < ApplicationController
   def create
     @chat = Chat.find(params[:chat_id])
-    @doc_contents = @chat.docs.first&.content
+    # @doc_contents = @chat.docs.first&.content
+    @doc_contents = @chat.docs
     @message = Message.new(message_params)
     @message.from_ai = false
     authorize @message
@@ -12,9 +13,15 @@ class MessagesController < ApplicationController
         @chat,
         render_to_string(partial: "message", locals: {message: @message})
       )
-      instructions = "Instructions: Respond to the following query in 40 words or less using information in the Contents. Try to make to the response as concise as possible and keep the word count as low as possible."
+      instructions = "Instructions: Respond to the following query in 40 words or less using information in the Contents. If there are more than one Contents, combine all of the contents together to provide a meaningful answer. Try to make to the response as concise as possible and keep the word count as low as possible."
       combined_text = instructions + "\n\n" + "Query: " + @message.contents.dup  # Prepend instructions to message contents
-      combined_text << "\n\n" + "Contents: " + @doc_contents if @doc_contents.present?
+      if @doc_contents.present?
+        doc_content_strings = @doc_contents.map { |doc| doc.content } # Assuming each doc has a "content" attribute
+        doc_contents_text = doc_content_strings.join("\n\n") # Join the content strings with newlines
+
+        combined_text << "\n\n" + "Contents: " + doc_contents_text
+      end
+      # combined_text << "\n\n" + "Contents: " + @doc_contents if @doc_contents.present?
 
       if combined_text.present?
         @response = OpenaiService.new(combined_text).call
