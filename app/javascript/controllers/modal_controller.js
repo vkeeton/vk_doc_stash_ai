f820afdf51f5c2c7e1af04ef4b15c50d620d2ed5
Zhen Luo <zhenluo1988@gmail.com>
2023-08-30 10:22:54 +0900

    the new chat modal can be scrolled now

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -7,8 +7,9 @@ export default class extends Controller {
   closeModal() {
     this.containerTarget.classList.add("hidden");
     this.newChatContainerTarget.classList.add("hidden");
-    this.element.nextElementSibling.classList.remove("active");
+    // this.element.nextElementSibling.classList.remove("active");
   }
+
   openNewChatModal() {
     this.newChatContainerTarget.classList.remove("hidden");
     this.element.nextElementSibling.classList.add("active");
